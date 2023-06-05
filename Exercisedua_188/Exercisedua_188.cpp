09e49ffb712d0434ffbbe9f217ddd93364935204
DeniArif <deniarifwibowo82@gmail.com>
2023-06-05 14:55:07 +0700

    comit

--- Exercisedua_188/Exercisedua_188.cpp
+++ Exercisedua_188/Exercisedua_188.cpp
@@ -4,11 +4,59 @@ using namespace std;
 
 class penerbit {
 public:
-	static int Pengarang;
-	int Penulis;
+	static int pengarang;
+	int DaftarPenerbit;
+	int DaftarBuku;
+	int DaftarBukuDikarang;
 	string nama;
 
-	void setPenulis();
+	void setDaftarPenerbit();
 	void printAll();
-	penerbit(string pNama) :nama(pNama) { setPenulis(); }
+	penerbit(string pNama) :nama(pNama) { setDaftarPenerbit(); }
+
+	void setDaftarBuku();
+	penerbit(string pNama) :nama(pNama) { setDaftarBuku(); }
+
+	void setDaftarBukuDikarang();
+	penerbit(string pNama) :nama(pNama) { setDaftarBukuDikarang(); }
 };
+
+int penerbit::pengarang = 0;
+
+
+void penerbit::setDaftarPenerbit() {
+	DaftarPenerbit = ++pengarang;
+}
+
+void penerbit::setDaftarBuku() {
+	DaftarBuku = ++pengarang;
+}
+
+void penerbit::setDaftarBukuDikarang() {
+	DaftarBukuDikarang = ++pengarang;
+}
+
+void penerbit::printAll() {
+	cout << " DaftarPenerbit Giga 1 = " << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+	cout << " DaftarBuku Joko = " << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+	cout << " DaftarBukuDikarang  = " << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	penerbit pnb1("Joko");
+	penerbit pnb2("Lia");
+	penerbit pnb3("Asroni");
+	penerbit pnb4("Giga");
+
+	pnb1.printAll();
+	pnb2.printAll();
+	pnb3.printAll();
+	pnb4.printAll();
+	return 0;
+}
